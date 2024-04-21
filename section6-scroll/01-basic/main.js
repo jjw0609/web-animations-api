cbb1a24e6f1c8d5de723c28e23b64303c728ce20
Jung JaeWoong <jjw_0609@naver.com>
2024-04-21 18:30:40 +0900

    스크롤 타임라인 이해하기2

--- section6-scroll/01-basic/main.js
+++ section6-scroll/01-basic/main.js
@@ -2,6 +2,7 @@ import './scroll-timeline.js';
 
 const progress = document.querySelector('.progress');
 const scrollBox = document.querySelector('.scroll-box');
+const targetImages = document.querySelectorAll('.target-image');
 
 progress.animate(
     [
@@ -19,3 +20,26 @@ progress.animate(
         })
     }
 );
+
+targetImages.forEach(image => {
+    const imageTop = image.offsetTop;
+    const imageHeight = image.offsetHeight;
+
+    const offset1 = imageTop + imageHeight - window.innerHeight;
+    const offset2 = imageTop - 200;
+
+    image.animate(
+        [
+            { opacity: 0 },
+            { opacity: 1 }
+        ],
+        {
+            timeline: new ScrollTimeline({
+                scrollOffsets: [
+                    new CSSUnitValue(offset1, 'px'),
+                    new CSSUnitValue(offset2, 'px')
+                ]
+            })
+        }
+    );
+});
